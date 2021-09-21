1b590b941de08517add5fcc1f6df37699e0a0002
beatriz <beatrizcostavi@gmail.com>
2021-09-20 23:58:52 -0300

    beatriz

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=aaa, initial-scale=1.0">
+    <title>projeto</title>
+</head>
+
+<body>
+    <h1>Aula 1</h1>
+
+</body>
+
+</html>
